6df746d399d4b315b9b875488da471662bb5e8f8
Ivam Santos <isantos@engebras.com.br>
2018-03-15 17:38:21 -0300

    Aulas VAR e LET

--- fundamentos/funcao1.js
+++ fundamentos/funcao1.js
@@ -1,1 +1,16 @@
 // função sem retorno
+function imprimirSoma(a, b){
+    console.log(a+b)
+}
+
+imprimirSoma(2,3)
+imprimirSoma(2) // Not a Number (NaN)
+imprimirSoma(2,10,4,5,6,7) // utiliza apenas 2 parametros e ignora os demais
+imprimirSoma() // Not a Number (NaN)
+
+function soma(a, b=1){
+    return a+b
+}
+console.log(soma(2,3))
+console.log(soma(2))
+console.log(soma()) // NaNisantos
