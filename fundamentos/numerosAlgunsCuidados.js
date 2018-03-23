8e55f4bfcdc1bde47d9633f7cdc83b2183fc3676
Ivam Henrique Marques dos Santos <ivamks@gmail.com>
2018-03-23 09:02:54 -0300

    Primeiras aulas

---
+++ fundamentos/numerosAlgunsCuidados.js
@@ -0,0 +1,7 @@
+console.log(7/0)
+console.log("10"/2)
+console.log('3' + 2)
+console.log("show"*2) // NaN
+console.log(0.1+0.7)  // Imprecisao.
+// console.log(10.toString())
+console.log((10).toString())
---
+++ fundamentos/variaveis.js
@@ -0,0 +1,17 @@
+var a = 3
+let b = 4
+
+var a = 30
+b = 40
+
+console.log(a,b)
+
+a = 300
+b = 4000
+
+console.log(a,b)
+
+const c = 5
+// c = 50 não pode atribuir novo valor
+
+console.log(c)
